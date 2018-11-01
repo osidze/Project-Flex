5df81c2760a0af26468ec462f6729b59662e0bcc
Olga <osidze12@gmail.com>
2018-11-01 14:20:51 +0200

    Project Flex-box

---
+++ site-template-init-project/assets/js/custom.js
@@ -0,0 +1,14 @@
+(function ($) {  
+
+
+  $(document).ready(function() {  
+      $("html").niceScroll();
+  });
+
+
+  $('.header-menu__btn').on('click', function () {
+    $(this).toggleClass('active');
+    $('.header-menu__list').toggleClass('active');
+  });
+
+})(jQuery);
